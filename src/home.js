9cc30cc38a0d3bc91285af0fa2296e31e5d9a09c
Ik3nna <iamiykendukwu@gmail.com>
2022-10-11 11:39:05 +0100

    Adding functionality

--- src/home.js
+++ src/home.js
@@ -1,11 +1,70 @@
 import react, { useState, useEffect } from "react";
 
 const Home = ()=> {
+    const [name, setName] = useState("");
+    const [list, setList] = useState([]);
+
+    const handleSubmit = (e)=> {
+        e.preventDefault();
+        
+        const newName = {id: new Date().getTime().toString(), title: name};
+        setList([...list, newName]);
+        setName("");
+    }
+
+    const removeTodo = (id) => {
+        list.filter((item)=> item.id !== id);
+    }
 
     return(
-        <main>
-            <section style={{ backgroundImage: "/images/bg-desktop-dark.jpg" }}>
-                   
+        <main className="top" style={{ 
+            backgroundImage: `url(${process.env.PUBLIC_URL + '/images/bg-desktop-dark.jpg'})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            height: "50vh"
+            }}>
+            <section>
+                <article className="text">
+                    <h1>TODO</h1> 
+
+                    <span>
+                        <img src="/images/icon-sun.svg" />
+                    </span>
+                </article>
+            
+            
+                <form onSubmit={handleSubmit}>
+                    <div className="circle"></div>
+                    <input type="text"
+                        value={name} 
+                        placeholder="Create a new todo..."
+                        onChange={(e)=>setName(e.target.value)} 
+                    />
+                </form>
+
+                <article className="form-list" style={{ display: list.length===0 ? "none": "block" }}>
+                    {list.map((item)=>{
+                        const { id, title } = item;
+                        return(
+                            <>
+                                <div key={id} className="list">
+                                    <div className="circle"></div> {title} 
+                                    <img src="/images/icon-cross.svg" onClick={()=>removeTodo(id)} />
+                                </div><hr />
+                            </>
+                        );
+                    })}
+
+                    <div className="end">
+
+                    </div>
+                </article>
+                    
+                <p>
+                    Drag and drop to reorder list
+                </p>
+
             </section>
         </main>
     );
