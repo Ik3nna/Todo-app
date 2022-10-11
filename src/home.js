3382e3ad68227f68cd036bea4c2e1830cca4b289
Ik3nna <iamiykendukwu@gmail.com>
2022-10-11 06:20:53 +0100

    initial setup

---
+++ src/home.js
@@ -0,0 +1,14 @@
+import react, { useState, useEffect } from "react";
+
+const Home = ()=> {
+
+    return(
+        <main>
+            <section style={{ backgroundImage: "/images/bg-desktop-dark.jpg" }}>
+                   
+            </section>
+        </main>
+    );
+}
+
+export default Home;
