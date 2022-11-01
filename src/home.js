66e6bca358c1629586ded8e84fac0e83e47195e3
Ik3nna <iamiykendukwu@gmail.com>
2022-11-01 13:29:36 +0100

    Finishing touches

--- src/home.js
+++ src/home.js
@@ -1,28 +1,32 @@
 import React, { useState, useEffect } from "react";
 
-// const getLocalStorage = ()=> {
-//     let list = localStorage.getItem("list");
+const getLocalStorage = ()=> {
+    let list = localStorage.getItem("list");
   
-//     if(list) {
-//       return JSON.parse(list);
-//     }
-//     else {
-//       return []
-//     }
-// }
+    if(list) {
+      return JSON.parse(list);
+    }
+    else {
+      return []
+    }
+}
 
 const Home = ()=> {
     const [name, setName] = useState("");
     const [toggle, setToggle] = useState(true);
 
-    const [list, setList] = useState([]);
+    const [list, setList] = useState([getLocalStorage()]);
     const [completeList, setCompleteList] = useState([]);
+    const [activeList, setActiveList] = useState([getLocalStorage()]);
+    const [displayActive, setDisplayActive] = useState(false);
+    const [displayCompleted, setDisplayCompleted] = useState(false);
+    const [check, setCheck] = useState(false);
     
     const handleSubmit = (e)=> {
         e.preventDefault();
         const newName = {id: new Date().getTime().toString(), title: name, completed: false};
         
-        if (newName.title == "") {
+        if (newName.title === "") {
             setList([]);
         } else {
             setList([...list, newName]);
@@ -40,44 +44,62 @@ const Home = ()=> {
         select.classList.add("active");
         
         list.map((item)=>{
-            if (item.title == select.textContent) {
+            if (item.title === select.textContent) {
                 let x = {...item, completed: item.completed = true}
                 return x;
             }
+            return item
         });
 
         setCompleteList(list.filter((item)=>
-            item.completed == true
+            item.completed === true
         ));
+
+        setDisplayActive(false)
     }
 
     const showAll = ()=> {
-        setList(list);
+        setActiveList(list);
+        setDisplayCompleted(false);
+        setDisplayActive(true);
     }
 
     const showActive = ()=> {
         let filteredList = list.filter((item)=>
-            item.completed == false
-        );
-       
-        setList(filteredList);
+            item.completed === false
+        )
+        setDisplayCompleted(false);
+        setActiveList(filteredList);
+        setDisplayActive(true);
+        setCompleteList([]);
     }
 
     const showCompleted = ()=> {
         let filteredList = list.filter((item)=>
-            item.completed == true
+            item.completed === true
         );
-
-        if (filteredList.length == 0) {
-            setList([])
-        } else {
-        setList(filteredList);
-        }
+        setDisplayCompleted(true);
+        setCompleteList(filteredList);
+        setDisplayActive(false);
+        setActiveList(list);
+        setCheck(true);
     }
 
+    const clearCompleted = ()=> {
+        let filteredList = list.filter((item)=>
+            item.completed === false
+        );
+        setList(filteredList);
+        setActiveList(filteredList);
+        setDisplayActive(true);
+        setCompleteList([]);
+        setDisplayCompleted(false);
+    }
 
     useEffect(()=>{
         localStorage.setItem("list",JSON.stringify(list));
+
+        setActiveList(list)
 
         const selectors = document.querySelectorAll("#selectors");
 
@@ -97,10 +119,17 @@ const Home = ()=> {
     },[list]);
 
     useEffect(()=>{
-        {toggle ? document.body.style.backgroundColor = "hsl(235, 21%, 11%)" : 
-            document.body.style.backgroundColor = "hsl(0, 0%, 98%)"
-        }
+        toggle ? document.body.style.backgroundColor = "hsl(235, 21%, 11%)" : 
+        document.body.style.backgroundColor = "hsl(0, 0%, 98%)"
+        
     },[toggle]);
+
+    useEffect(()=>{
+        const checked = document.querySelectorAll("#checkbox");
+        checked.forEach((item)=>
+            item.checked = true
+        );
+    },[check])
 
     return(
         <main className={`${toggle ? "dark" : "light"}`} style={{ 
@@ -119,8 +148,8 @@ const Home = ()=> {
 
                     <span onClick={()=>setToggle(!toggle)}>
                         {toggle ? 
-                            <img  src="/images/icon-sun.svg" /> :
-                            <img src="/images/icon-moon.svg" />
+                            <img  src="/images/icon-sun.svg" alt="icon-sun" /> :
+                            <img src="/images/icon-moon.svg" alt="icon-moon" />
                         }
                     </span>
                 </article>
@@ -136,25 +165,25 @@ const Home = ()=> {
                 </form>
 
                 <article className="form-list" style={{ display: list.length===0 ? "none": "block" }}>
-                    {list.map((item)=>{
+                    {(displayCompleted ? completeList : activeList).map((item)=>{
                         const { id, title } = item;
                         return(
                             <React.Fragment key={id}>
                                 <div className="list">
-                                    <input type="radio" onClick={handleComplete} />{title} 
+                                    <input type="checkbox" id="checkbox" onClick={handleComplete} />{title} 
 
-                                    <img src="/images/icon-cross.svg" onClick={()=>removeTodo(id)} />
+                                    <img src="/images/icon-cross.svg" alt="icon-cross" onClick={()=>removeTodo(id)} />
                                 </div><hr />
                             </React.Fragment>
                         );
                     })}
 
                     <div className="end">
-                        <p>{completeList.length == 0 ? list.length : (list.length - completeList.length)} items left</p>
+                        <p>{completeList.length === 0 ? activeList.length : (activeList.length - completeList.length)} items left</p>
                         <p onClick={showAll} className="color all" id="selectors">All</p>
                         <p onClick={showActive} id="selectors">Active</p>
                         <p onClick={showCompleted} id="selectors">Completed</p>
-                        <p onClick={showActive} id="selectors">Clear Completed</p>
+                        <p onClick={clearCompleted} id="selectors">Clear Completed</p>
                     </div>
                 </article>
 
